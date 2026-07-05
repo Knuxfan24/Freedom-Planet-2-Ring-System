cd1ff6d85578cff8f85970e1b898f98d54bc6392
Knux <knuxfan@live.co.uk>
2026-07-05 20:44:22 +0100

    Update Plugin.cs
    Noting down an oversight.

--- Plugin.cs
+++ Plugin.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 
 namespace Freedom_Planet_2_Ring_System
 {
+    // TODO: Fix drowning and heat damage doing nothing.
     [BepInPlugin("K24_FP2_Rings", "Ring System", "1.0.0")]
     [BepInDependency("000.kuborro.libraries.fp2.fp2lib")]
     public class Plugin : BaseUnityPlugin
